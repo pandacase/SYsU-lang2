d97bd0fa624985a525f446142ab1c6ba0e2ccb73
pandacase <2227119810@qq.com>
2024-03-09 20:42:17 +0800

    line:index process

--- task/1/antlr/main.cpp
+++ task/1/antlr/main.cpp
@@ -1,7 +1,8 @@
-#include "SYsU_lang.h" // make sure the name is same as .g4
+#include "SYsU_lang.h" // make sure the name is same as `.g4`
 #include <fstream>
 #include <iostream>
 #include <unordered_map>
+#include <regex>
 
 // definition mapping，ANTLR tokenTypeName -> clang format
 std::unordered_map<std::string, std::string> tokenTypeMapping = {
@@ -24,11 +25,27 @@ std::unordered_map<std::string, std::string> tokenTypeMapping = {
   // more...
 };
 
+//! @brief 
+//! 
+//! @param token 
+//! @param tokens 
+//! @param outFile 
+//! @param lexer 
+//! @param lineBias 
+//! @param fileLoc 
+//! @param gotSpace 
+//! @param withStart 
 void print_token(
-      const antlr4::Token* token,
-      const antlr4::CommonTokenStream& tokens,
-      std::ofstream& outFile,
-      const antlr4::Lexer& lexer) {
+  const antlr4::Token* token,
+  const antlr4::CommonTokenStream& tokens,
+  std::ofstream& outFile,
+  const antlr4::Lexer& lexer,
+  const int lineBias,
+  const std::string& fileLoc,
+  const bool gotSpace,
+  const bool withStart
+) {
+  
   auto& vocabulary = lexer.getVocabulary();
 
   auto tokenTypeName =
@@ -40,23 +57,99 @@ void print_token(
   if (tokenTypeMapping.find(tokenTypeName) != tokenTypeMapping.end()) {
     tokenTypeName = tokenTypeMapping[tokenTypeName];
   }
-  std::string locInfo = " Loc=<0:0>";
 
+  // check if [StartOfLine]
+  const int lineNumber = token->getLine() - lineBias;
+  const int indexNumber = token->getCharPositionInLine() + 1;
   bool startOfLine = false;
-  bool leadingSpace = false;
+  if (indexNumber == 1 || withStart) {
+    startOfLine = true;
+  }
 
-  if (token->getText() != "<EOF>")
+  // check if [LeadingSpace]
+  bool leadingSpace = gotSpace;
+
+  // load location info
+  std::string locInfo = " Loc=<./";
+  locInfo.append(fileLoc);
+  locInfo.append(":");
+  locInfo.append(std::to_string(lineNumber));
+  locInfo.append(":");
+  locInfo.append(std::to_string(indexNumber));
+  locInfo.append(">");
+
+  // token info output
+  if (token->getText() != "<EOF>") {
     outFile << tokenTypeName << " '" << token->getText() << "'";
-  else
+    if (startOfLine) {
+      outFile << "\t [StartOfLine]";
+    }
+    
+    if (leadingSpace) {
+      outFile << " [LeadingSpace]";
+    }
+  } else {
     outFile << tokenTypeName << " '"
             << "'";
-  if (startOfLine)
-    outFile << "\t [StartOfLine]";
-  if (leadingSpace)
-    outFile << " [LeadingSpace]";
+  }
+
   outFile << locInfo << std::endl;
 }
 
+//! @brief 
+//! 
+//! @param lexer 
+//! @param tokens 
+//! @param outFile 
+void print_tokens(
+  SYsU_lang& lexer,
+  antlr4::CommonTokenStream& tokens,
+  std::ofstream& outFile
+) {
+  int LinesOfPreprocessing = 0;
+  std::string fileLoc;
+  bool gotSpace = false, withStart = false;
+  for (auto&& token : tokens.getTokens()) {
+    if (token->getChannel() == lexer.HIDDEN) {
+      if (token->getType() == lexer.LineAfterPreprocessing) {
+        LinesOfPreprocessing += 1;
+        // only at the first time can read the file location
+        if (LinesOfPreprocessing == 1) {
+          std::string input = token->getText();
+          std::regex pattern("\"\\/workspaces\\/SYsU-lang2\\/test\\/cases\\/(.+?)\"");
+          std::smatch match;
+          if (std::regex_search(input, match, pattern)) {
+            fileLoc = match[1];
+          }
+        }
+      } else if (token->getType() == lexer.Whitespace) {
+        gotSpace = true;
+        if (token->getCharPositionInLine() == 0) {
+          withStart = true;
+        }
+      } else if (token->getType() == lexer.Newline) {
+        gotSpace = false;
+        withStart = false;
+      }
+    } else {
+      print_token(
+        token, tokens, outFile, lexer, 
+        LinesOfPreprocessing, fileLoc, 
+        gotSpace, withStart
+      );
+      if (gotSpace) {
+        gotSpace = false;
+        if (withStart) {
+          withStart = false;
+        }
+      }
+    }
+  }
+}
+
+
+//! @brief 
+//! 
 int main(int argc, char* argv[]) {
   if (argc != 3) {
     std::cout << "Usage: " << argv[0] << " <input> <output>\n";
@@ -85,7 +178,5 @@ int main(int argc, char* argv[]) {
   antlr4::CommonTokenStream tokens(&lexer);
   tokens.fill();
 
-  for (auto&& token : tokens.getTokens()) {
-    print_token(token, tokens, outFile, lexer);
-  }
+  print_tokens(lexer, tokens, outFile);
 }
